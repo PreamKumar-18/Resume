e22ee85433dbdb5f84082212ca64a22b67aac71e
Pream Kumar M <Pream.Kumar@svam.com>
2023-05-09 18:36:55 +0530

    Hi

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ function Home() {
             <div className='container'>
                 <div className='row'>
                     <div className='col s12 m3'>
-                        <Profile />
+                        <Profile /> Hello
                     </div>
                     <div className='col s12 m9'>
                         <About />
